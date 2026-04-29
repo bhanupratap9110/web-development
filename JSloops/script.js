99264e3e66659ab3dda82a804219c1800e6449c3
bhanupratap9110 <bhanupratap911010@gmail.com>
2026-04-29 23:31:24 +0530

    JS-itteration-statement

---
+++ JSloops/script.js
@@ -0,0 +1,29 @@
+for(var x = 2; x<=20; x+=2){
+
+    console.log(x);
+}
+
+var y = 2;
+while (y<=20){
+    console.log(y);
+    y++;
+}
+
+var z = 2;
+do{
+    console.log(z);
+    z++;
+
+}while (z<=20);
+
+
+// let a = [24,58,33,86,96,12,53,23,32,100]
+// for(let i = 0; i<10; i++){
+//     console.log(a[i]);
+// }
+
+// let i = 0;
+// while(i<10){
+//     console.log(a[i]);
+//     i++;
+// }
