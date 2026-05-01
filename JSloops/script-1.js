b3a656e9da12fef49023a1b0c111454b9667ec06
bhanupratap9110 <bhanupratap911010@gmail.com>
2026-05-01 18:26:09 +0530

    JS-loops-assignment-1

---
+++ JSloops/script-1.js
@@ -0,0 +1,36 @@
+for(var a = 1; a<=5; a++){
+
+    console.log(a);
+}
+for(var b = 1; b<=10; b++){
+
+    console.log(b);
+}
+for(var c = 10; c>=1; c--){
+
+    console.log(c);
+}
+for(var d = 2; d<=10; d+=2){
+
+    console.log(d);
+}
+for(var e = 1; e<=9; e+=2){
+
+    console.log(e);
+}
+for(var f = 1; f<=10; f++){
+
+    console.log(15*f);
+}
+for(var g = 1; g<=20; g += 2){
+
+    console.log(g);
+}
+for(var h = 5; h<=50; h += 2){
+
+    console.log(h);
+}
+for(var i = 1; i<=10; i++){
+    if(i % 3 ==0)
+    console.log(i);
+}
