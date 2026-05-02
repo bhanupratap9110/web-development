448226cbcecf685284f2d6af667f35a6cd322f78
bhanupratap9110 <bhanupratap911010@gmail.com>
2026-05-02 18:21:55 +0530

    Conditional-Statement

---
+++ JS-Conditional-Statement/sript-1.js
@@ -0,0 +1,48 @@
+//  Q no. 5
+let n = 5;
+if (n>0){
+    console.log("positive integer");
+    }else{
+        console.log("negative integer");
+    }
+ // Q no.6
+let A=49;
+if(A>90){
+    console.log("A")
+}
+ else if(A>75 && A<90){
+    console.log("B");
+}
+ else if(A>50 && A<75){
+    console.log("C")
+}
+else{
+    console.log("Fail");
+}
+
+// Q no.7
+let user = "abcd";
+let pass = "12345";
+
+user === "abcd" && pass === "12345"
+?console.log("login pass")
+:console.log("login fail");
+
+// Q no. 8
+let a = 1,
+b = 5;
+console.log(a > 2 || b < 3);
+
+// Q NO. 9
+// short-hand
+let data;
+let name = "bhanu";
+
+data = name || "N/A"
+console.log(data);
+
+// note
+let x = 2;
+console.log(x>10 && y<5);
+
+// Switch case
