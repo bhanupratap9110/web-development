80a315495700a25e0c7f678824382ad687abef47
bhanupratap9110 <bhanupratap911010@gmail.com>
2026-05-03 12:32:32 +0530

    JS-methods

---
+++ JS-methods/script-1.js
@@ -0,0 +1,36 @@
+// Sum 2 digite
+let a = 20 , b = 30;
+function sum (x,y){
+let z = x+y
+console.log(z);
+}
+sum (a,b)
+
+// sum
+let add = function(a , b){
+    return a + b;
+};
+console.log(add);
+console.log(add(4 , 6));
+
+// function expression
+// let ke saath nhi const likhna hai 
+const  abc = function(){
+    console.log("Hello world");
+
+};
+// callback function
+// console.log(abc)
+console.log(abc);
+
+//Arrow function
+console.log(add(4 , 6));
+
+console.log(add());
+const arrowfun = () => {
+    console.log("I am Arrow Function");
+};
+// Arrow function-ADD
+arrowfun();
+const addarrow = (a,b) => a+b;
+console.log(addarrow(4+5));
