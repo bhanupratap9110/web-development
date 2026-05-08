e9588543bc65e3f08abb66ba977d128c0f9be469
bhanupratap9110 <bhanupratap911010@gmail.com>
2026-05-08 18:32:16 +0530

    JS-Event-Handelling

---
+++ JS-Event-Handelling/script-1.js
@@ -0,0 +1,15 @@
+function Bulb1ON(){
+    document.getElementById("bulb1").style.backgroundColor = "yellow";
+}
+
+function Bulb1OFF(){
+    document.getElementById("bulb1").style.backgroundColor = "white";
+}
+
+function Bulb2ON(){
+    document.getElementById("bulb1").style.backgroundColor = "green";
+}
+function Bulb2OFF(){
+    document.getElementById("bulb1").style.backgroundColor = "Blue";
+}
+
