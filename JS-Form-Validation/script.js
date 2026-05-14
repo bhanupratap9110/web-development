b63252ffeedaa150aba705a6cf587a6fb3e5fe69
bhanupratap9110 <bhanupratap911010@gmail.com>
2026-05-14 19:26:02 +0530

    JS-Form-Validation

---
+++ JS-Form-Validation/script.js
@@ -0,0 +1,22 @@
+// function submit()
+// {
+//     let name = document.getElementById("fullName").value;
+//     let number = document.getElementById("phone").value;
+//     let email = document.getElementById("email").value;
+//     let password = document.getElementById("password").value;
+//     console.log(name);
+//     console.log(number);
+//     console.log(email);
+//     console.log(password);
+
+//     if(name>="A" && name<="Z")
+//     {
+        
+//         console.log(name);
+//     }
+//     else
+//     {
+//         alert("cannot write number into name field")
+//     }
+
+// }
