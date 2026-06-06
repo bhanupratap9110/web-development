b20a06b116648d37089059a49ee93570e98e4408
bhanupratap9110 <bhanupratap911010@gmail.com>
2026-06-06 20:33:25 +0530

    Remove unused files and update ContactUs, Login, and Register components with form handling

--- React/MYFIRSTREACTAPP/src/pages/ContactUs.jsx
+++ React/MYFIRSTREACTAPP/src/pages/ContactUs.jsx
@@ -1,7 +1,71 @@
+import { useState } from "react";
 function ContactUs(){
+    const [userName, setUserName] = useState("");
+        const [password, setPassword] = useState("");
+        const [Email, setEmailID] = useState("");
+        const [Mobile, MobileNumber ] = useState("");
+        
+          const handleSubmit = (e) => {
+            e.preventDefault();
+        
+            console.log("userName :", userName);
+            console.log("password :", password);
+            console.log("Email :", setEmailID);
+            console.log("MobileNumber :", MobileNumber);
+          };
     return(
         <>
-        <div>ContactUS</div>
+        <div className="container d-flex justify-content-center align-items-center">
+      </div>
+      <div className="d-flex justify-content-center">
+        <div className="border p-3 w-50 mt-5 bg-light shadow rounded">
+          <h1 className="text-center">Contact-US</h1>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="userName">FullName</label>
+            <input
+              type="text"
+              name="userName"
+              className="form-control"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <br />
+            <label htmlFor="password">Mobile Number</label>
+            <input
+              type="text"
+              name="Mobile"
+              className="form-control"
+              value={Email}
+              onChange={(e) => setMobileNumber(e.target.value)}
+            />
+            <br />
+            <label htmlFor="password">email ID</label>
+            <input
+              type="text"
+              name="Email-ID"
+              className="form-control"
+              value={Email}
+              onChange={(e) => setEmailID(e.target.value)}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              name="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <div className="float-end">
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
         </>
     );
 }
